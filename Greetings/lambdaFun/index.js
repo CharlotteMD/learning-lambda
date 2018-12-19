cae2632c30dd46563e50a8ceaaddc23e6a4989e2
CharlotteMD <charlottemdavies@hotmail.co.uk>
2018-12-19 12:00:40 +0000

    added response from an api

--- Greetings/lambdaFun/index.js
+++ Greetings/lambdaFun/index.js
@@ -1,6 +1,9 @@
 // this is the handler function which handles the user input 
 // you pass it an event which in context passes or fails (line 15)
 
+var http = require('http');
+// this is inbuilt in Node.js
+
 exports.handler = function(event, context) {
 // the event is linked to event.json file
 
@@ -33,21 +36,27 @@ exports.handler = function(event, context) {
 
             // alter the greeting depending on the time of day
             options.speechText += getWish(); 
-            options.endSession = true;
-            // closes app after this exchange
+            getQuote(function(quote, err) {
+                // if there is an error, Alexa won't try to read it!
+                if(err) {
+                    context.fail(err);
+                } else {
+                    options.speechText += quote; 
+                    options.endSession = true;
+                    // closes app after this exchange
+                    context.succedd(buildResponse(options));
+                }
+            });
 
-            context.succedd(buildResponse(options));
         } else {
             // this skill only has one intent - HelloIntent
             context.fail("Unknown intent") 
+        } else (request.type === "SessionEndedRequest") {
+
+        } else {
+            context.fail("Unknown intent type")
         }
-
-    } else (request.type === "SessionEndedRequest") {
-
-    } else {
-        context.fail("Unknown intent type")
     }
-}
 
 function getWish() {
     var myDate = new Date();
@@ -65,6 +74,36 @@ function getWish() {
     } else {
         return "Good evening. ";
     }
+}
+
+function getQuote(callback) {
+    // can take data from API to include in response
+    var url = "http://api.forismatic.com/api/1.0/json?method=getQuote&lang=en&format=json";
+
+    // get a random quote from this quote API
+    var req = http.get(url, function(res) {
+        var body = "";
+
+        // every time we get a response from the API, the chunk of data that we get will be passed to the body
+        res.on('data', function(chunk) {
+            body += chunk;
+        });
+
+        // after we have the data, trigger this 'end' function 
+        res.on('end', function() {
+            // this replaces any symbols within the data to spaces (which Alexa can read)
+            body = body.replace(/\\/g, '');
+            // this converts the data we get back into JSON
+            var quote = JSON.parse(body);
+            // from this data, take just the quoteText
+            callback(quote.quoteText)
+        })
+    });
+
+    // if there are errors getting the data from the API, trigger this callback to log the error
+    req.on('error', function(err) {
+        callback('', err);
+    });
 }
 
 function buildResponse(options) {
@@ -88,5 +127,5 @@ function buildResponse(options) {
             // this comes from the response file in the response object, labelled reprompt
 
         }
-    }
+    };
 }
